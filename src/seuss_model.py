c14ae10c395401db20543ab7bb60840f9ecb9c21
lledson425 <lledson@middlebury.edu>
2026-05-13 21:03:33 -0400

    Update seuss_model.py

--- src/seuss_model.py
+++ src/seuss_model.py
@@ -10,7 +10,7 @@ Original file is located at
 import torch
 import torch.nn as nn
 
-class SeuussModel(nn.Module):
+class SeussModel(nn.Module):
     def __init__(self, vocab_size, context_length, embedding_dim, hidden_dim):
         super().__init__()
         self.context_length = context_length
